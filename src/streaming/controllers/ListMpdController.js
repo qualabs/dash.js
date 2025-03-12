48692ead5a379d88bbe4625f2bbe00a4901fb92d
Constanza Dibueno <constanzad@qualabs.com>
2025-03-12 15:00:22 -0300

    bug fix: some elements of imported period always overrides an element of the linked period

--- src/streaming/controllers/ListMpdController.js
+++ src/streaming/controllers/ListMpdController.js
@@ -117,9 +117,9 @@ function ListMpdController() {
         const updatedManifest = new Promise(resolve => {
             manifestLoader.load(resolvedUri, null, null, true)
                 .then((importedManifest) => {
-                    dashAdapter.mergeManifests(manifest, importedManifest, period.id, mpdHasDuration);
+                    dashAdapter.mergeManifests(manifest, importedManifest, period, mpdHasDuration);
                 }, () => {
-                    dashAdapter.mergeManifests(manifest, null, period.id, mpdHasDuration);
+                    dashAdapter.mergeManifests(manifest, null, period, mpdHasDuration);
                 })
                 .then(() => {
                     eventBus.trigger(Events.MANIFEST_UPDATED, { manifest });
