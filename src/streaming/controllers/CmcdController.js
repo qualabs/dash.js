cf96bad2e987765a767f016031628979c8cb1f15
Constanza Dibueno <constanzad@qualabs.com>
2025-05-15 11:34:28 -0300

    add warning for empty event array

--- src/streaming/controllers/CmcdController.js
+++ src/streaming/controllers/CmcdController.js
@@ -160,23 +160,28 @@ function CmcdController() {
     function triggerCmcdEventMode(event){
         const targets = settings.get().streaming.cmcd.targets;
         const eventModeTargets = targets.filter((target) => target.cmcdMode === Constants.CMCD_MODE.EVENT);
-        
+
         if (eventModeTargets.length === 0) {
             return;
         }
-        
+
         const cmcdData = {
             ..._getGenericCmcdData(),
             ..._updateMsdData(Constants.CMCD_MODE.EVENT),
             e: event
         };
-
+        
         if (event == 'e') {
             cmcdData.ec = internalData.ec;
         }
         
         eventModeTargets.forEach(targetSettings => {
             if (targetSettings.enabled) {
+
+                if (targetSettings.events.length === 0) {
+                    logger.warn('CMCD Event Mode is enabled, but the "events" setting is empty. No event-specific CMCD data will be sent.');
+                }
+
                 let events = targetSettings.events ? targetSettings.events : Object.values(Constants.CMCD_REPORTING_EVENTS);
 
                 if (!events.includes(event)) {
